ba183c3e2b346ce027a165f28a9a22e9956a8464
darshangit <darshans1989@gmail.com>
2018-07-28 16:50:42 +0530

    integration with mongo DB complete

--- server/models/author.js
+++ server/models/author.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
-const bookSchema = new Schema({
-    
+//id is not required mongoDB is going to craete one for us
+const authorSchema = new Schema({
+    name: String,
+    age: Number,
 })
+
+module.exports = mongoose.model('Author', authorSchema);
