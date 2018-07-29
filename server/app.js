d382629584ea17043da9776e5691c0e39fe4bf12
darshangit <darshans1989@gmail.com>
2018-07-29 12:45:53 +0530

    add book component + applo client integration with node serve

--- server/app.js
+++ server/app.js
@@ -2,8 +2,12 @@ const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schemas = require('./schema/schema')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express();
+
+//allow cross-origin requests
+app.use(cors())
 
 mongoose.connect('mongodb://dash:dash123@ds261040.mlab.com:61040/gql-ninja',{ useNewUrlParser: true })
 mongoose.connection.once('open', () => {
